b5d0ee5615ac89d84b4be81541b6876eda256a53
Muhammad-ArifinDev <ipin93936@gmail.com>
2025-11-04 21:29:02 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+//import 'screen/splash_screen_1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "NIM : 1123150053, arifin",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      ),
+     // home: const MySplashScreen(), // pastikan nama class cocok
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
